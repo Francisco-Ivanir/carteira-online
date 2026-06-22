0a517b6c1c4cdca34e7b1e9a18c54cbd9f2852aa
Francisco-Ivanir <ficfivanir@outlook.com>
2026-06-22 16:45:11 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,37 @@
+function fazerLogin() {
+
+const usuario =
+document.getElementById("usuario").value;
+
+const senha =
+document.getElementById("senha").value;
+
+if (
+usuario === "admin" &&
+senha === "123456"
+) {
+
+```
+localStorage.setItem(
+  "adminLogado",
+  "sim"
+);
+
+window.location.href =
+  "admin.html";
+```
+
+} else {
+
+```
+alert(
+  "Usuário ou senha inválidos."
+);
+```
+
+}
+
+}
+
+window.fazerLogin =
+fazerLogin;
