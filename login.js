0aff88723147bb16b3ac29ed5a571e33759c6893
Francisco-Ivanir <ficfivanir@outlook.com>
2026-06-23 12:42:36 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,41 @@
-alert("ARQUIVO NOVO");
-
 function fazerLogin() {
 
-alert("BOTAO FUNCIONOU");
+const usuario =
+document.getElementById("usuario")
+.value
+.trim();
+
+const senha =
+document.getElementById("senha")
+.value
+.trim();
+
+if (
+usuario === "admin" &&
+senha === "123456"
+) {
+
+```
+localStorage.setItem(
+  "adminLogado",
+  "sim"
+);
+
+window.location.href =
+  "admin.html";
+```
+
+} else {
+
+```
+alert(
+  "Usuário ou senha inválidos."
+);
+```
 
 }
 
-window.fazerLogin = fazerLogin;
+}
+
+window.fazerLogin =
+fazerLogin;
