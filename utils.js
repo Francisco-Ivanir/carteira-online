d6436f16648a963e14448ec4ad228bfd80c383f0
Francisco-Ivanir <ficfivanir@outlook.com>
2026-06-26 22:33:42 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,22 @@
-tu
+export function formatarMoeda(valor) {
+
+return "R$ " +
+Number(valor)
+.toLocaleString("pt-BR");
+
+}
+
+export function calcularPercentual(
+pago,
+emprestado
+) {
+
+if (!emprestado) {
+return 0;
+}
+
+return (
+pago / emprestado
+) * 100;
+
+}
